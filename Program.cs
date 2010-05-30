b9d18741be6d872fd95b92a52107f5aa77f6b20d
Jaddie <james@jaddie.co.uk>
2010-05-30 16:37:59 +0100

    Split off the linus commands.

--- Program.cs
+++ Program.cs
@@ -649,34 +649,6 @@ namespace Jad_Bot
         #endregion
 
 
-        #region Nested type: AddLinusTorvaldsFactCommand
-
-        public class AddLinusTorvaldsFactCommand : Command
-        {
-            public AddLinusTorvaldsFactCommand()
-                : base("al", "addlinus", "addtorvalds")
-            {
-                Usage = "ac Linus fact here.";
-                Description = "Add a Linus Torvalds fact to storage";
-            }
-
-            public override void Process(CmdTrigger trigger)
-            {
-                try
-                {
-                    var norris = new StreamWriter("LinusFacts.txt", true) {AutoFlush = false};
-                    norris.WriteLine(trigger.Args.Remainder);
-                    trigger.Reply("Added the new Linus Torvalds fact: {0} to storage", trigger.Args.Remainder);
-                    norris.Close();
-                }
-                catch(Exception e)
-                {
-                    Print(e.Data + e.StackTrace,true);
-                }
-            }
-        }
-
-        #endregion
 
         #region Nested type: AttackCommand
 
@@ -1214,40 +1186,6 @@ namespace Jad_Bot
         #endregion
 
 
-        #region Nested type: RandomLinusTorvaldsFactCommand
-
-        public class RandomLinusTorvaldsFactCommand : Command
-        {
-            public RandomLinusTorvaldsFactCommand()
-                : base("rl", "linus", "torvalds")
-            {
-                Usage = "rl";
-                Description = "Get a random fact about Linus Torvalds";
-            }
-
-            public override void Process(CmdTrigger trigger)
-            {
-                try
-                {
-                    var linusLines = new List<string>();
-                    var linus = new StreamReader("LinusFacts.txt");
-                    while (!linus.EndOfStream)
-                    {
-                        linusLines.Add(linus.ReadLine());
-                    }
-                    var rand = new Random();
-                    int randnum = rand.Next(0, linusLines.Count - 1);
-                    trigger.Reply(linusLines[randnum]);
-                    linus.Close();
-                }
-                catch(Exception e)
-                {
-                    Print(e.Data + e.StackTrace,true);
-                }
-            }
-        }
-
-        #endregion
 
         #region Nested type: ReadSourceFile
 
