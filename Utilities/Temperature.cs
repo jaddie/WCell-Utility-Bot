8cc235e0ebaed8f2a9cc20af8ec8f88060691e5d
Jaddie <james@jaddie.co.uk>
2010-06-09 15:55:04 +0100

    Google is stupid, this is actually the correct spelling.
    Removed redundant tostring.

--- Utilities/Temperature.cs
+++ Utilities/Temperature.cs
@@ -7,12 +7,12 @@ using Squishy.Irc.Commands;
 
 namespace Jad_Bot.Utilities
 {
-    class Celcius : Command
+    class Celsius : Command
     {
-                public Celcius()
-            : base("celcius")
+                public Celsius()
+            : base("celsius")
         {
-            Usage = "celcius degree";
+            Usage = "celsius degree";
             Description = "returns celsius converted to other measurements";
         }
 
@@ -20,11 +20,11 @@ namespace Jad_Bot.Utilities
         {
             try
             {
-                int celcius = Convert.ToInt32(trigger.Args.Remainder.Trim());
-                int fahrenheit = (celcius * 9 / 5) + 32;
-                int kelvin = celcius + 273;
+                int celsius = Convert.ToInt32(trigger.Args.Remainder.Trim());
+                int fahrenheit = (celsius * 9 / 5) + 32;
+                int kelvin = celsius + 273;
 
-                trigger.Reply(trigger.Args.Remainder + " Celcius is , " + fahrenheit.ToString() + "F , " + kelvin.ToString() + " Kelvin");
+                trigger.Reply(trigger.Args.Remainder + " Celsius is , " + fahrenheit.ToString() + "F , " + kelvin.ToString() + " Kelvin");
             }
             catch(Exception e)
             {
@@ -47,10 +47,10 @@ namespace Jad_Bot.Utilities
             try
             {
                 int fahrenheit = Convert.ToInt32(trigger.Args.Remainder.Trim());
-                int celcius = (fahrenheit - 32) * 5 / 9;
+                int celsius = (fahrenheit - 32) * 5 / 9;
                 int kelvin = (fahrenheit + 459) * 5 / 9;
 
-                trigger.Reply(trigger.Args.Remainder + " Fahrenheit is , " + celcius + "C , " + kelvin + " Kelvin");
+                trigger.Reply(trigger.Args.Remainder + " Fahrenheit is , " + celsius.ToString() + "C , " + kelvin.ToString() + " Kelvin");
             }
             catch (Exception e)
             {
