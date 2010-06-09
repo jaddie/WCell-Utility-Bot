8467858bd6db223b8d7248525fcf8435b433713f
failers <tom_englund@hotmail.com>
2010-06-09 17:18:31 +0200

    added two new commands pastebay, imgbay

--- FunCommands/LegalBay.cs
+++ FunCommands/LegalBay.cs
@@ -29,4 +29,48 @@ namespace Jad_Bot
             }
         }
     }
+
+    class PasteBay : Command
+    {
+        public PasteBay()
+            : base("paste", "pastebay")
+        {
+            Usage = "pastebay";
+            Description = "returns link to pastebay";
+        }
+
+        public override void Process(CmdTrigger trigger)
+        {
+            try
+            {
+                trigger.Reply("If you have more than 2 lines to show us, please paste them at http://wcell.pastebay.com/ and show us the link.");
+            }
+            catch (Exception e)
+            {
+                UtilityMethods.Print(e.Data + e.StackTrace, true);
+            }
+        }
+    }
+
+    class BayImg : Command
+    {
+        public BayImg()
+            : base("BayImg")
+        {
+            Usage = "bayimg";
+            Description = "returns link to bayimg";
+        }
+
+        public override void Process(CmdTrigger trigger)
+        {
+            try
+            {
+                trigger.Reply("If you have a picture you like show us or perhaps a file you want to send us .rar it and upload to http://bayimg.com/ and show us the link.");
+            }
+            catch (Exception e)
+            {
+                UtilityMethods.Print(e.Data + e.StackTrace, true);
+            }
+        }
+    }
 }
