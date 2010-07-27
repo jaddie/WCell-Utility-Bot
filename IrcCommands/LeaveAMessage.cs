adf18d411c4eec5b86423269b1612721d60c76e9
James <james@jaddie.co.uk>
2010-07-28 00:27:22 +0100

    Sigh, git sucks so much hg would easily have added this file..

---
+++ IrcCommands/LeaveAMessage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Squishy.Irc.Commands;
+
+namespace Jad_Bot.IrcCommands
+{
+    class LeaveAMessage : Command
+    {
+        public LeaveAMessage() : base("leavemsg","lm","note")
+        {
+            Usage = "leavemsg nicktoleavefor message to send.";
+            Description = "Use to leave a message for someone on IRC for when they come back online";
+        }
+        public override void Process(CmdTrigger trigger)
+        {
+            var nick = trigger.Args.NextWord();
+            var messagetosend = trigger.Args.Remainder;
+            if (string.IsNullOrEmpty(nick) || string.IsNullOrEmpty(messagetosend))
+            {
+                trigger.Reply("Failed to parse input, please try again");
+            }
+            else
+            {
+              //  using(var UtilityBotDBContainer();
+            }
+        }
+    }
+}
