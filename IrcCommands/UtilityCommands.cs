1dd3e3342bf355d93a198f792f61f119be2f57b8
James <james@jaddie.co.uk>
2010-07-28 00:58:34 +0100

    Moved Leave a message, updated file name

---
+++ IrcCommands/UtilityCommands.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Squishy.Irc.Commands;
+
+namespace Jad_Bot.IrcCommands
+{
+    class UtilityCommands
+    {
+        public class LeaveAMessage : Command
+        {
+             public LeaveAMessage() : base("leavemsg","lm","note")
+             {
+                 Usage = "leavemsg nicktoleavefor message to send.";
+                 Description = "Use to leave a message for someone on IRC for when they come back online";
+             }
+             public override void Process(CmdTrigger trigger)
+             {
+                 var nick = trigger.Args.NextWord();
+                 var messagetosend = trigger.Args.Remainder;
+                 if (string.IsNullOrEmpty(nick) || string.IsNullOrEmpty(messagetosend))
+                 {
+                     trigger.Reply("Failed to parse input, please try again");
+                 }
+                 else
+                 {
+                     using (var db = new UtilityBotDBContainer())
+                     {
+                         var msg = new Message();
+                         msg.DateLeft = DateTime.Now.ToString();
+                         msg.IrcNick = nick;
+                         msg.MessageText = messagetosend;
+                         db.Messages.AddObject(msg);
+                         db.SaveChanges();
+                         trigger.Reply("Message saved");
+                         return;
+                     }
+                     trigger.Reply("Failed to save the message for some reason.");
+                 }
+             }
+        }
+    }
+}
